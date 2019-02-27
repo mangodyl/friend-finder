59cd9bf5a114577440e45ddfcd1b5f980229aa45
mango dyl <dylanbonds@hotmail.com>
2019-02-27 17:06:40 -0600

    js files mostly done, html begun

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+
+var friendsArray = [
+    {
+        "name": "",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/51Pe8UNUiVL._SX466_.jpg",
+        "scores": [
+
+        ]
+    }, {
+        "name": "",
+        "photo": "",
+        "scores": [
+
+        ]
+    },
+];
+
+module.exports = friendsArray;
