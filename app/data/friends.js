a8f26cd8cecb599bcb3e5f6a474897c26ec0db78
mango dyl <dylanbonds@hotmail.com>
2019-02-28 14:38:17 -0600

    survey filled & inputs posted

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,34 @@
 
 var friendsArray = [
     {
-        "name": "",
+        "name": "Sultry Skeleton",
         "photo": "https://images-na.ssl-images-amazon.com/images/I/51Pe8UNUiVL._SX466_.jpg",
         "scores": [
-
+            5,
+            1,
+            4,
+            3,
+            2,
+            1,
+            5,
+            1,
+            5,
+            3
         ]
     }, {
-        "name": "",
-        "photo": "",
+        "name": "Violent Giraffe",
+        "photo": "https://dailylolpics.com/wp-content/uploads/2018/05/Random-Pictures-Of-The-Day-20-05-2018-15.jpg",
         "scores": [
-
+            1,
+            1,
+            4,
+            4,
+            5,
+            3,
+            2,
+            4,
+            4,
+            5
         ]
     },
 ];
