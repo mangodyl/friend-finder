20eb3ed6263710de9186a4d9860d71aa34bddd07
mango dyl <dylanbonds@hotmail.com>
2019-02-28 18:32:29 -0600

    compare friends math complete

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,5 +8,35 @@ module.exports = function(app) {
   
     app.post("/api/friends", function(req, res) {
         friendsData.push(req.body);
+
+        console.log(`${req.body} <- body
+        ${req.body.scores} <- scores
+        ${friendsData} <- friends data
+        ${friendsData[0].scores[0]}`)
+
+
+
+        for (let i = 0; i < friendsData.length; i++) {
+            let diffArray = [];
+            let bestDiff = 0;
+            let bestIndex = 0;
+            for (let j = 0; j < 10; j++) {
+                let diff = (req.body.scores[j]) - (friendsData[i].scores[j])
+                diffArray.push(diff);
+                if (j === 9) {
+                    const totalDiff = diffArray.reduce((acc, val) => {
+                        acc + val;
+                    })
+                    if (totalDiff > bestDiff) {
+                        bestDiff = totalDiff;
+                        bestIndex = i;
+                    };
+                };
+            };
+            if(i === (friendsData.length - 1)) {
+                console.log(friendsData[bestIndex].name);
+            }
+        };
+
     });
   };
